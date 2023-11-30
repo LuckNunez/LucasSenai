0017dd5b3a93ce0d8faefafa153df2f76dafa5d9
LuckNunez <lucasnunes71@hotmail.com>
2023-11-30 20:27:44 -0300

    commit Exemplo While e For
    Signed-off-by: LuckNunez <lucasnunes71@hotmail.com>

---
+++ exemplo.py
@@ -0,0 +1,22 @@
+# Exemplos de Utilização For e While
+
+#  //Exemplo Utilizando o For//
+# # for i in range(5):
+# #     print(i)
+
+# //Exemplo Utilizando o While//
+# i = 0
+# while i <10:
+#     print(i)
+#     i += 1
+
+# //Teste de For //
+# Notas = [90,85,78,92,88,76,95,89] # Notas dos alunos
+
+# Soma = 0
+
+# for Nota in Notas:
+#     Soma += Nota
+
+# Media = Soma/len(Notas)
+# print("A Média das Notas é: ", Media)
