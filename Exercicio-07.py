e7cf00a8aa16e62bde8f09ff521631e287628604
LuckNunez <lucasnunes71@hotmail.com>
2023-12-06 21:12:25 -0300

    Commit Exercicio-07
    Signed-off-by: LuckNunez <lucasnunes71@hotmail.com>

---
+++ Exercicio-07.py
@@ -0,0 +1,18 @@
+# Importação com library Pandas
+# Realizando importações com bibliotecas utilizando o recurso "VENV"
+
+import pandas as pd
+
+dados = {
+    'Nome': ['Lucas','Samuel','Davi','Jadson'],
+    'Idade': [22,19,18,21],
+    'Cidade': ['Camaçari','São Paulo','Salvador','Rio de Janeiro'],
+}
+
+#Conversão para pandas "tabela = pd.DataFrame(dados)"
+df = pd.DataFrame(dados)
+#print (tabela)
+
+for dado in df.values:
+    #print(dado)
+    print(dado[0],dado[1],dado[2])
